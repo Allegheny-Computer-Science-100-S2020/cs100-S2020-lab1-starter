93103352a80a106a599eba6e0dac8f5b3bbdccd7
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-01-23 10:19:59 -0500

    Add the starting version of the DisplayOutput.java.

---
+++ src/main/java/labone/DisplayOutput.java
@@ -0,0 +1,40 @@
+package labone;
+
+// TODO: Import the correct package to access the system's date
+
+// TODO: Make sure to add your name as the programmer for this Java class
+
+/** This class displays four lines of output.
+ * The first line displays the name of the programmer and the date. Each one of
+ * the next three lines displays a single statement created by the programmer.
+ *
+ * <p>Bugs: This program has no known bugs, except those purposefully inserted.
+ *
+ * @author Add Your Name Here
+ */
+
+public class DisplayOutput {
+
+  /** The main entry point for DisplayOutput.
+   *  The source code inside of this method will be executed when the program is
+   *  run in the command-line. The output of this program will appear in the
+   *  terminal window. The println statements make the program display output.
+   *
+   * @param args The command-line arguments
+   **/
+  public static void main(String[] args) {
+
+    // TODO: Display the date using the Date constructor
+    System.out.println("Add Your Name Here");
+    // TODO: Display three insightful lines
+    // TODO: Make sure that the commands are in
+    //       the order that will produce the expected output
+    // TODO: Study the README.md file to see the expected output
+    // TODO: Include source code comments to explain the code's behavior
+    System.out.println("Docker is Dynamite.");
+    System.out.println("Gradle is Great.");
+    System.out.println("Hello World.");
+
+  }
+
+}
